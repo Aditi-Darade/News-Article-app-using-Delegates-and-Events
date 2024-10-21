98ed48a4b6d8b723c002abbc46a7d5de487d0ccc
Aditi <aditidarade1@gmail.com>
2024-10-21 13:28:12 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class NewsArticle
+    {
+        public string Title;
+        public string Category;
+
+        public NewsArticle(string title, string category)
+        {
+            Title = title; 
+            Category = category; 
+        }
+
+    }
+
+    public class NewsArticleCollection
+    {
+
+       public List<NewsArticle> articles = new List<NewsArticle>();
+
+
+        public void AddArticle(NewsArticle article)
+        {          
+            articles.Add(article);
+            //ArticleAdded();
+        }
+
+        public void RemoveArticle(NewsArticle article)
+        {
+            articles.Remove(article);
+        }
+
+        public void FilterArticlesByCategory(string category)
+        {
+
+            foreach(NewsArticle itObj in articles)
+            {
+                if (itObj.Category == category)
+                {
+
+                    Console.WriteLine(itObj.Title + ", " + itObj.Category);
+                }
+            }
+        }
+        //a delegate that is invoked when a new article is added to the collection
+        public delegate void articleAddedHandler();
+ 
+        public void ArticleAdded()
+        {
+            Console.WriteLine("Article was added");
+        }
+    }
+
+}
---
+++ Sec20_Assign41_NewsApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ClassLibrary1.NewsArticleCollection;
+
+namespace Sec20_Assign41_NewsApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            NewsArticle n1 = new NewsArticle("aaa", "boring");
+            NewsArticle n2 = new NewsArticle("bbb", "fun");
+            NewsArticle n3 = new NewsArticle("ccc", "fun");
+            NewsArticle n4 = new NewsArticle("ddd", "fun");
+
+            NewsArticleCollection newsArticleCollection = new NewsArticleCollection();
+
+            articleAddedHandler articleAdded = new articleAddedHandler(newsArticleCollection.ArticleAdded);
+
+
+            newsArticleCollection.AddArticle(n1);
+            articleAdded.Invoke();
+            System.Console.WriteLine(newsArticleCollection.articles[0].Title + ", " + newsArticleCollection.articles[0].Category);
+ 
+            newsArticleCollection.AddArticle(n2);
+            articleAdded.Invoke();
+            System.Console.WriteLine(newsArticleCollection.articles[1].Title + ", " + newsArticleCollection.articles[1].Category);
+            newsArticleCollection.RemoveArticle(n2);
+
+            newsArticleCollection.AddArticle(n3);
+            System.Console.WriteLine(newsArticleCollection.articles[1].Title + ", " + newsArticleCollection.articles[1].Category);
+
+            newsArticleCollection.AddArticle(n4);
+            System.Console.WriteLine(newsArticleCollection.articles[2].Title + ", " + newsArticleCollection.articles[2].Category);
+
+            int listCount = newsArticleCollection.articles.Count;
+            System.Console.WriteLine("List Count = " + listCount);
+            System.Console.WriteLine("-------------------");
+
+            newsArticleCollection.FilterArticlesByCategory("fun");
+            System.Console.WriteLine("-------------------");
+
+            //articleAddedHandler articleAdded = new articleAddedHandler(newsArticleCollection.ArticleAdded);
+            articleAdded.Invoke();
+      
+            System.Console.ReadKey();
+        }
+    }
+}
